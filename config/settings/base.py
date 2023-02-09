1dd57fdb3455f9ead1d04154013540e3b10405d4
Reza <reza1831amin@gmail.com>
2023-02-09 13:49:59 +0330

    create unit test for products

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 
     # 3 party
     'rest_framework',
+    'drf_spectacular',
+
     # ! local
     'products',
 ]
@@ -131,3 +133,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Django ecommerce',
+}
